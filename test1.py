89a50167fcc1f43796c876dbd2ce0644b9600dd5
GartDev <30609938+GartDev@users.noreply.github.com>
2017-07-31 21:10:04 -0400

    Update test1.py

--- test1.py
+++ test1.py
@@ -1,13 +1,14 @@
-for i in range(0,10):
+#A vendor has 9 apples hes willing to give to you, he counts apples, ask how many you want, then responds based on user input
+for i in range(0,10): #He's not smart he has to count them individually
 	if(i == 1):
-		print("i have " + str(i) + " apple")
+		print("i have " + str(i) + " apple") #exception for 1 apple
 	else:
-		print("i have " + str(i) + " apples")
+		print("i have " + str(i) + " apples")  #everything else
 print("how many apples do you want?")
-a = input()
-if(int(a) < 10):
+a = input() #let him know how many you want
+if(int(a) < 10): #make sure he has the apples u want
 	print("alright, here you go")
-else:
+else: #you asked for too many
 	print("I don't have that many apples")
 
 	
